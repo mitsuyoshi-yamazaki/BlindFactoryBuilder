4211e8c3b655febf4f07f0cb83b438f5f5832168
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2018-03-20 02:24:28 +0900

    Get initial blueprint and deploy it

--- control.lua
+++ control.lua
@@ -7,33 +7,74 @@ DEBUG = true
 --
 
 script.on_event(defines.events.on_tick, function(event)
-  if game.tick % 30 == 0 then
-    check_missing_construction_object_alert()
+  if game.tick % 60 == 0 then
+    for _, player in pairs(game.players) do
+      blind_factory_builder(player)
+    end
   end
 end)
 
 -- Functions
 
-function check_missing_construction_object_alert()
-  for _, player in pairs(game.players) do
-    local missing_construction_object_alerts = player.get_alerts{}[0][3]
+function blind_factory_builder(player)
+  if global.blueprints ~= nil then
+    return
+  end
 
-    logTo(player, missing_construction_object_alerts)
+  local blueprints = get_init_blueprints(player)
+  if blueprints == nil then 
+    return
+  end
 
-    if next(missing_construction_object_alerts) == nil then
-      break
-    end
+  global.blueprints = blueprints[2]
 
-    for _, alert in pairs(missing_construction_object_alerts) do
-      local missing_object_type = alert.target.ghost_prototype.type
-      logTo(player, "Missing "..missing_object_type)
+  log_to(player, "blueprints set")
 
+  global.blueprints.build_blueprint{surface=player.surface, force=player.force, position={x=0, y=0}}
+  --check_missing_construction_object_alert(player)
+end
 
-    end
+function get_init_blueprints(player)
+  local selected_entity = player.selected
+  if selected_entity == nil then
+    return
+  end
+  if selected_entity.type ~= "container" then
+    return
+  end
+
+  local inventory = selected_entity.get_inventory(defines.inventory.chest)
+  if inventory[1].valid_for_read == false then 
+    return
+  end
+  if inventory[1].label ~= "__init__" then
+    return
+  end
+
+  log_to(player, "blueprints set")
+  return inventory
+end
+
+function check_missing_construction_object_alert(player)
+  local missing_construction_object_alerts = player.get_alerts{}[0][3]
+
+  if next(missing_construction_object_alerts) == nil then
+    return
+  end
+
+  for _, alert in pairs(missing_construction_object_alerts) do
+    local missing_object_type = alert.target.ghost_prototype.type
+    --log_to(player, "Missing "..missing_object_type)
+
+    construct(player, missing_object_type)
   end
 end
 
-function logTo(player, message)
+function construct(player, object_type)
+
+end
+
+function log_to(player, message)
   if DEBUG then
     player.print(serpent.block(message))
 
